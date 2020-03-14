3d369d6872d8544eff592412f8e823e0ef9c34f6
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-14 07:07:50 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,12 +12,14 @@ def happy_birthday(birthday_kids)
   end
 end
 
+
+
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     if age <= 12
       puts "Happy Birthday #{name}! You are now #{age} years old!"
-    else
-      "You are too old for this."
+    elsif age > 12
+      puts "You are too old for this."
     end
   end
 end
